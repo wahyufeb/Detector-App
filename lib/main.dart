fcb60a1b3b11871cfff11aa838580008f3258e67
Wahyu Febrianto <febriantowahyu63@gmail.com>
2020-11-21 19:27:30 +0700

    Add simple splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,102 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:gender_detector_app/main_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ));
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool data;
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  Future<Timer> loadData() async {
+    return Timer(Duration(seconds: 3), onDoneLoading);
+  }
+
+  onDoneLoading() async {
+    Navigator.of(context)
+        .pushReplacement(MaterialPageRoute(builder: (context) => MainPage()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainPage(),
+    return Scaffold(
+      body: Container(
+        color: Color.fromRGBO(148, 66, 212, 1),
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: const Offset(0, 60),
+                          blurRadius: 50,
+                          color: Color(0x7090B0).withAlpha(60),
+                        ),
+                      ],
+                    ),
+                    child: ClipRRect(
+                      child: Image(
+                        image: AssetImage("assets/icon.png"),
+                        width: 200,
+                        height: 200,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Detector App",
+                          style: TextStyle(
+                            fontFamily: "Poppins",
+                            fontWeight: FontWeight.w700,
+                            fontSize: 30,
+                            color: Colors.white.withOpacity(0.8),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Versi 1.1 ~ Wahyu Febrianto",
+                  style: TextStyle(
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w300,
+                    fontSize: 15,
+                    color: Colors.white.withOpacity(0.5),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main_page.dart
+++ lib/main_page.dart
@@ -4,90 +4,92 @@ import 'package:gender_detector_app/home_page.dart';
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(
-        padding: EdgeInsets.all(30),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [Colors.blue[600], Colors.purple[300]],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "Aplikasi ini dibuat hanya untuk hiburan \n 😊",
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                  fontFamily: "Poppins",
+    return Scaffold(
+      body: Material(
+        child: Container(
+          padding: EdgeInsets.all(30),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.blue[600], Colors.purple[300]],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Aplikasi ini dibuat hanya untuk hiburan \n 😊",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: "Poppins",
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25),
-                  boxShadow: [
-                    BoxShadow(
-                      offset: const Offset(0, 10.0),
-                      blurRadius: 20.0,
-                      color: Colors.pink.withAlpha(51),
-                      // spreadRadius: 2.0,
-                    ),
-                  ],
+                SizedBox(
+                  height: 50,
                 ),
-                child: Material(
-                  elevation: 0,
-                  borderRadius: BorderRadius.circular(20),
-                  child: Container(
-                    width: 150,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          colors: [Colors.pinkAccent[100], Colors.pink[400]],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Material(
-                      color: Colors.transparent,
-                      borderRadius: BorderRadius.circular(20),
-                      child: InkWell(
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(0, 10.0),
+                        blurRadius: 20.0,
+                        color: Colors.pink.withAlpha(51),
+                        // spreadRadius: 2.0,
+                      ),
+                    ],
+                  ),
+                  child: Material(
+                    elevation: 0,
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      width: 150,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [Colors.pinkAccent[100], Colors.pink[400]],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter),
                         borderRadius: BorderRadius.circular(20),
-                        splashColor: Colors.pink,
-                        child: Center(
-                          child: Text(
-                            "Lanjut",
-                            style: TextStyle(
-                              color: Colors.white.withOpacity(0.9),
-                              fontWeight: FontWeight.w500,
-                              fontFamily: "Poppins",
+                      ),
+                      child: Material(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(20),
+                        child: InkWell(
+                          borderRadius: BorderRadius.circular(20),
+                          splashColor: Colors.pink,
+                          child: Center(
+                            child: Text(
+                              "Lanjut",
+                              style: TextStyle(
+                                color: Colors.white.withOpacity(0.9),
+                                fontWeight: FontWeight.w500,
+                                fontFamily: "Poppins",
+                              ),
                             ),
                           ),
+                          onTap: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return HomePage();
+                                },
+                              ),
+                            );
+                          },
                         ),
-                        onTap: () {
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return HomePage();
-                              },
-                            ),
-                          );
-                        },
                       ),
                     ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
